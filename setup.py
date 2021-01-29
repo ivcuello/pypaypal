de16b5ee7f8c5649a3d80efa4104851134e96bc8
ivcuello <ivcuello@gmail.com>
2021-01-29 18:38:53 -0400

    updating license template on setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2020 Tetsumune KISO
+Copyright (C) 2020 Ivan Cuello
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
